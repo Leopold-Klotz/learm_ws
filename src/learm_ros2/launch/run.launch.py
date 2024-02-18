c66e516adb80907519c78d2f932d5490e83ba667
Leopold-Klotz <leopoldk3@gmail.com>
2024-02-18 14:21:44 -0800

    Added modeling for the hand

--- src/learm_ros2/launch/run.launch.py
+++ src/learm_ros2/launch/run.launch.py
@@ -4,20 +4,26 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
+import xacro
 
 def generate_launch_description():
     gui = LaunchConfiguration('gui', default='true')
     
     rviz_config = os.path.join(
         get_package_share_directory('learm_ros2_description'),
-        'rviz/learm.rviz')
+        'rviz/hand_learm.rviz')
 
-    urdf = os.path.join(
+    # urdf_path = os.path.join(
+    #     get_package_share_directory('learm_ros2_description'),
+    #     'urdf/learm.urdf')
+    urdf_path_xacro = os.path.join(
         get_package_share_directory('learm_ros2_description'),
-        'urdf/learm.urdf')
+        'urdf/newLearm.urdf.xacro')
+    robot_desc = xacro.process_file(urdf_path_xacro).toxml()
+    urdf_path = urdf_path_xacro
 
-    with open(urdf, 'r') as infp:
-        robot_desc = infp.read()
+    # with open(urdf_path, 'r') as infp:
+    #     robot_desc = infp.read()
 
     return LaunchDescription([
         DeclareLaunchArgument(
@@ -28,13 +34,13 @@ def generate_launch_description():
             executable='joint_state_publisher' + '_gui' if gui else '',
             name='joint_state_publisher',
             parameters=[{'robot_description': robot_desc}],
-            arguments=[urdf]),
+            arguments=[]),
         Node(
             package='robot_state_publisher',
             executable='robot_state_publisher',
             name='robot_state_publisher',
             parameters=[{'robot_description': robot_desc}],
-            arguments=[urdf]),
+            arguments=[]),
         Node(
             package='rviz2',
             executable='rviz2',
